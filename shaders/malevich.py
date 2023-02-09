6a451b8b39788457ab46c3a24b182dced1bdb46e
Valentin <valentin7petr@gmail.com>
2023-02-09 21:10:36 +0300

    Initial commit

---
+++ shaders/malevich.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+
+
+def pyshader(func, w, h):
+    scr = bytearray((0, 0, 0) * w * h)
+    for y in range(h):
+        for x in range(w):
+            p = (w * y + x) * 3
+            scr[p:p + 3] = [max(min(int(c * 255), 255), 0)
+                            for c in func(x / w, y / h)]
+    return bytes('P6\n%d %d\n255\n' % (w, h), 'ascii') + scr
+
+
+# Ваш код здесь:
+def func(x, y):
+    if 0.1 <= x <= 0.9 and 0.1 <= y <= 0.9:
+        return 0, 0, 0
+
+    return 1, 1, 1
+
+
+label = tk.Label()
+img = tk.PhotoImage(data=pyshader(func, 256, 256)).zoom(2, 2)
+label.pack()
+label.config(image=img)
+tk.mainloop()
